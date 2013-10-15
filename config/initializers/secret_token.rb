6fc5c549d296ed95eaeaa8719b3e560ca80cac49
Eric VanWieren <eric@codex.org>
2013-10-14 23:38:58 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omr::Application.config.secret_key_base = '1b41263468621d5fec07f77e8e837c7799094c8142d681e680b9a3bd40e45dc2b63d9791b77929f4a9c1da747990654a6a62d7429bbe556d87347f5a4bfe58a0'
